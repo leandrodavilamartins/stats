f968b57e948a5340dd9d2cf7521f6f5a3f6e9d4b
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2021-11-14 20:12:15 -0300

    added Binomial and Poisson classes

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const Data = require('./stats')
+const Statistics = require('./stats')
 
-let numbers = [6,5,4,3,2,1]
+const poisson = new Statistics.Poisson(10)
+const bin = new Statistics.Binomial(10, 0.3,0.7)
+//let numbers = [6,5,4,3,2,1]
 
-let stat = new Data(numbers);
+//let stat = new Data(numbers);
 
 //console.log(stat.data);
 // console.log(stat.media());
 // console.log(stat.variancia());
 // console.log(stat.cv());
 // console.log(stat.contador());
-console.log(stat.desvio);
-console.log(stat.moda);
-console.log(stat.mediana);
-console.log(stat.percentil(1));
-console.log(stat.media);
+//console.log(stat.desvio);
+//console.log(stat.moda);
+//console.log(stat.mediana);
+//console.log(stat.percentil(1));
+//console.log(stat.media);
+console.log(poisson.probability(5));
+console.log(bin.probability(4));
